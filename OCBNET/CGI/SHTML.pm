326cbc6432fc24696107301f911838b5323353c2
Marcel Greter <marcel.greter@ocbnet.ch>
2014-12-03 23:22:50 +0100

    Fork namespace

--- OCBNET/CGI/SHTML.pm
+++ OCBNET/CGI/SHTML.pm
@@ -1,11 +1,11 @@
 $VERSION = "1.29";
-package CGI::SHTML;
+package OCBNET::CGI::SHTML;
 our $VERSION = "1.29";
 
-# -*- Perl -*-		Wed May 19 13:09:58 CDT 2004 
+# -*- Perl -*-		Wed May 19 13:09:58 CDT 2004
 #############################################################################
 # Written by Tim Skirvin <tskirvin@ks.uiuc.edu>
-# Copyright 2001-2004, Tim Skirvin and UIUC Board of Trustees.  
+# Copyright 2001-2004, Tim Skirvin and UIUC Board of Trustees.
 # Redistribution terms are below.
 #############################################################################
 
@@ -22,7 +22,7 @@ CGI::SHTML - a CGI module for parsing SSI
   print $cgi->header();
   print $cgi->start_html('internal', -title=>"SAMPLE PAGE");
   # Insert content here
-  print $cgi->end_html('internal', -author=>"Webmaster", 
+  print $cgi->end_html('internal', -author=>"Webmaster",
 		        -address=>'webserver@ks.uiuc.edu');
 
   # Just parse some SSI text
@@ -39,7 +39,7 @@ Further functionality is documented with the CGI module.
 
 In order to parse SSI, you generally have to configure your scripts to be
 re-parsed through Apache itself.  This module eliminates that need by
-parsing SSI headers itself, as best it can.   
+parsing SSI headers itself, as best it can.
 
 Some information on SSI is available at
 B<http://www.cclabs.missouri.edu/things/instruction/www/html/xssi.shtml>.
@@ -86,17 +86,17 @@ $EMPTY 	   = "";	# Edit this for debugging
 # Set some environment variables that are important for SSI
 $ENV{'DATE_GMT'}      = gmtime(time);
 $ENV{'DATE_LOCAL'}    = localtime(time);
-$ENV{'DOCUMENT_URI'}  = join('', "http://", 
-			 $ENV{'SERVER_NAME'} || "localhost", 
+$ENV{'DOCUMENT_URI'}  = join('', "http://",
+			 $ENV{'SERVER_NAME'} || "localhost",
 			 $ENV{'SCRIPT_NAME'} || $0 ) ;
-$ENV{'LAST_MODIFIED'} = CGI::SHTML->_flastmod( $ENV{'SCRIPT_FILENAME'} || $0 );
+$ENV{'LAST_MODIFIED'} = __PACKAGE__->_flastmod( $ENV{'SCRIPT_FILENAME'} || $0 );
 delete $ENV{'PATH'};
 
 @ISA = "CGI";
 
-if ( -r $CONFIG ) { do $CONFIG } 
+if ( -r $CONFIG ) { do $CONFIG }
 
-=head2 SUBROUTINES 
+=head2 SUBROUTINES
 
 =over 2
 
@@ -107,29 +107,29 @@ performs the various local functions that are necessary.
 
 =cut
 
-sub new { 
-  my $item = CGI::new(@_); 
-  $$item{'NOPRINT'} = [];  
-  $$item{'IFDONE'} = [];  
+sub new {
+  my $item = CGI::new(@_);
+  $$item{'NOPRINT'} = [];
+  $$item{'IFDONE'} = [];
   $$item{'IF'} = 0;
-  bless $item, shift; $item; 
+  bless $item, shift; $item;
 }
 
 =item parse_shtml ( LINE [, LINE [, LINE ]] )
 
-Parses C<LINE> as if it were an SHTML file.  Returns the parsed set of 
-lines, either in an array context or as a single string suitable for 
+Parses C<LINE> as if it were an SHTML file.  Returns the parsed set of
+lines, either in an array context or as a single string suitable for
 printing.  All of the work is actually done by C<ssi()>.
 
 =cut
 
 sub parse_shtml {
   my ($self, @lines) = @_;
-  map { chomp } @lines; my $line = join("\n", @lines); 
+  map { chomp } @lines; my $line = join("\n", @lines);
   my @parts = split m/(<!--#.*?-->)/s, $line;
 
-  my @return; 
-  while (@parts) { 
+  my @return;
+  while (@parts) {
     my @ssi = ();
     my $text = shift @parts || "";
     unless ($self->_noprint) {
@@ -137,7 +137,7 @@ sub parse_shtml {
     }
     if (scalar @parts && $parts[0] =~ /^<!--#(\w+)\s*(.*)?-->\s*$/m) {
       @ssi = ($1, $2); shift @parts;
-    } 
+    }
     my $ssival = $ssi[0] ? $self->ssi(@ssi) : undef;
     unless ($self->_noprint) {
       push @return, $ssival if defined $ssival && $ssival ne '';
@@ -181,9 +181,9 @@ sub ssi {
   while ($args) { 		# Parse $args
     $args =~ s/^(\w+)=(\"[^\"]*\"|'.*'|\S+)\s*//;
     last unless defined($1);
-    my $item = lc $1; my $val = $2;  
-    $val =~ s/^\"|\"$//g; 
-    $hash{$item} = $val if defined($val); 
+    my $item = lc $1; my $val = $2;
+    $val =~ s/^\"|\"$//g;
+    $hash{$item} = $val if defined($val);
   }
 
   my $orig = $self->_noprint;
@@ -194,22 +194,22 @@ sub ssi {
     my $val = _ssieval(\%hash);
     if ($val) { $self->_noprint(0); $self->_ifdone(1); }
     else      { $self->_noprint(1); }
-  
+
     my $noprint = $self->_noprint;
     return "";
 
-  } elsif (lc $command eq 'else') { 
-    if ($self->_ifdone) { $self->_noprint(1); } 
+  } elsif (lc $command eq 'else') {
+    if ($self->_ifdone) { $self->_noprint(1); }
     else               { $self->_noprint(0); $self->_ifdone(1); }
     my $noprint = $self->_noprint;
     return "";
 
-  } elsif (lc $command eq 'endif') { 
+  } elsif (lc $command eq 'endif') {
     my $noprint = $self->_noprint(0);
     my $ifdone  = $self->_ifdone(0);
     $$self{'IF'}--;
     return "";
-  } 
+  }
 
   if (lc $command eq 'include') {
     if ( defined $hash{'virtual'} ) { $self->_file(_vfile( $hash{'virtual'} )) }
@@ -217,7 +217,7 @@ sub ssi {
     else { return "No filename offered" };
   } elsif (lc $command eq 'set') {
     my $var = $hash{'var'} || return "No variable to set";
-    my $value = $hash{'value'} || ""; 
+    my $value = $hash{'value'} || "";
     $value =~ s/\{(.*)\}/$1/g;
     $value =~ s/^\$(\S+)/$ENV{$1} || $EMPTY/egx;
     $ENV{$var} = $value;
@@ -230,15 +230,15 @@ sub ssi {
     if    ( defined $hash{'cmd'} ) { $self->_execute( $hash{'cmd'} ) || ""  }
     elsif ( defined $hash{'cgi'} ) { $self->_execute( _vfile($hash{'cgi'}) ) }
     else { return "No filename offered" };
-  } elsif (lc $command eq 'fsize') { 
+  } elsif (lc $command eq 'fsize') {
     if    ( defined $hash{'virtual'}) { $self->_fsize(_vfile($hash{'virtual'}))}
     elsif ( defined $hash{'file'})    { $self->_fsize( $hash{'file'} ) }
     else { return "No filename offered" };
-  } elsif (lc $command eq 'flastmod') { 
+  } elsif (lc $command eq 'flastmod') {
     if (defined $hash{'virtual'})  { $self->_flastmod(_vfile($hash{'virtual'}))}
     elsif ( defined $hash{'file'}) { $self->_flastmod( $hash{'file'} ) }
     else { return "No filename offered" };
-  } else { return "" } 
+  } else { return "" }
 }
 
 =item start_html ( TYPE, OPTIONS )
@@ -254,11 +254,11 @@ C<$CGI::SHTML::HEADER>.
 sub start_html {
   my ($self, $type, %hash) = @_;
   $type = lc $type;  $type ||= 'default';
-  
+
   foreach my $key (keys %hash) {
     if (lc $key eq '-title') { $ENV{'TITLE'} = $hash{$key} }
   }
-  
+
   my $command = "<!--#include virtual=\"$HEADER{$type}\"-->";
 
   return join("\n", CGI->start_html(\%hash), $self->parse_shtml($command) );
@@ -274,7 +274,7 @@ C<CGI::end_html>.
 sub end_html {
   my ($self, $type, %hash) = @_;
   $type = lc $type;  $type ||= 'default';
-  
+
   my $command = "<!--#include virtual=\"$FOOTER{$type}\"-->";
 
   join("\n", $self->parse_shtml($command), CGI->end_html(\%hash));
@@ -298,17 +298,17 @@ sub _vfile {
   # If it starts with a '$' sign, then get the value out first
   if ($filename =~ /^\$\{?(\S+)\}?$/) { $filename = $ENV{$1} || ""; }
 
-  my $hostname = $ENV{'HTTP_HOST'} || $ENV{'HOSTNAME'};  
+  my $hostname = $ENV{'HTTP_HOST'} || $ENV{'HOSTNAME'};
   foreach my $replace (keys %REPLACE) {
-    next if ($hostname =~ /^www/);	# Hack 
+    next if ($hostname =~ /^www/);	# Hack
     $filename =~ s%$replace%$REPLACE{$replace}%g;
   }
   my $newname;
-  if ($filename =~ m%^~(\w+)/(.*)$%) { $newname = "/home/$1/public_html/$2"; } 
-  elsif ( $filename =~ m%^[^/]% ) { 
+  if ($filename =~ m%^~(\w+)/(.*)$%) { $newname = "/home/$1/public_html/$2"; }
+  elsif ( $filename =~ m%^[^/]% ) {
     my ($directory, $program) = $0 =~ m%^(.*)/(.*)$%;
-    $newname = "$directory/$filename" 
-  } 
+    $newname = "$directory/$filename"
+  }
   else { $newname = "$ROOTDIR/$filename" }
   $newname =~ s%/+%/%g;  # Remove doubled-up /'s
   $newname;
@@ -338,8 +338,8 @@ sub _execute {
   map { chomp } @list;
   return "" unless scalar(@list) > 0;	# Didn't return anything
   # Take out the "Content-type:" part, if it's a CGI - note, THIS IS A HACK
-  if ( scalar(@list) > 1 && $list[0] =~ /^Content-type: (.*)$/i) { 
-    shift @list;  shift @list; 
+  if ( scalar(@list) > 1 && $list[0] =~ /^Content-type: (.*)$/i) {
+    shift @list;  shift @list;
   }
   wantarray ? @list : join("\n", @list);
 }
@@ -348,7 +348,7 @@ sub _execute {
 ## _fsize( FILE )
 # Last modification and file size of the given FILE, respectively.
 sub _flastmod { localtime( (stat($_[1]))[9] || 0 ); }
-sub _fsize    { 
+sub _fsize    {
   my $size = ((stat($_[1]))[7]) || 0;
   if ($size >= 1048576) {
     sprintf("%4.1fMB", $size / 1048576);
@@ -365,10 +365,10 @@ sub _fsize    {
 # dangerous, though, since it uses 'eval'.  Then again, given that we're
 # already giving the user the capacity to invoke random pieces of code,
 # it's not realy that much of a stretch...
-sub _ssieval { 
+sub _ssieval {
   my $hash = shift;
   if (my $var  = $$hash{'var'})  { return $var ? 1 : 0 }
-  if (my $eval = $$hash{'expr'}) { 
+  if (my $eval = $$hash{'expr'}) {
     $eval =~ s/\s*\$(?:\{(\S+?)\}|(\S+?))\s*
 	      / join('', "'", $ENV{$1 || $2} || "", "'" ) /egx;
     my $val = eval($eval);
@@ -389,7 +389,7 @@ This module was generated for a single research group at UIUC.  Its goal
 was simple: parse the SSI header and footers that were being used for the
 rest of the web site, so that they wouldn't have to be re-implemented
 later.  Ideally, we would liked to just have Apache take care of this, but
-it wasn't an option at the time (and as far as I know it still isn't one.)  
+it wasn't an option at the time (and as far as I know it still isn't one.)
 
 I mention the above because it's worth understanding the problem before
 you think about its limitations.  This script will not offer particularly
@@ -447,43 +447,43 @@ Tim Skirvin <tskirvin@ks.uiuc.edu>.
 ### Version History ###########################################################
 ###############################################################################
 # v1.0 		Thu Apr 13 13:30:30 CDT 2000
-### Documented it, and put this module into its proper home.  
+### Documented it, and put this module into its proper home.
 # v1.1 		Thu Apr 20 09:25:28 CDT 2000
 ### Updated for new page layout, included better counter capabilities, and
-### put in the possiblity of hooks for when we need to update this for all 
+### put in the possiblity of hooks for when we need to update this for all
 ### of the web pages.
 # v1.11 	Thu Apr 20 13:48:28 CDT 2000
 ### Further updates, added NOCOUNTER flag for error messages
 # v1.12 	Tue Apr 25 13:28:15 CDT 2000
 ### More updates of the header/footer files
 # v1.2 		Tue Jun 13 09:42:11 CDT 2000
-### Now just parses the header/footer files from the main directory, and 
+### Now just parses the header/footer files from the main directory, and
 ### includes a "parse_shtml" function set.  Hopefully at some point I'll
 ### finish off parse_shtml to do all SSI functions.
 # v1.21 	Wed Jun 28 10:56:26 CDT 2000
 ### Fixed the CGI handlings to trim out the Content-type header.
 # v1.22 	Wed Oct 31 09:46:16 CST 2001
 ### Fixed _vfile() to do local directory checks properly.
-### Changed execute() behaviour to not worry about tainting - probably a 
+### Changed execute() behaviour to not worry about tainting - probably a
 ###   bad idea, but necessary for now.
 # v1.23 	Mon Dec 10 11:58:25 CST 2001
 ### Created $EMPTY.  Updated 'set' to use variables in its code.
 # v1.24 	Tue Apr  2 13:05:12 CST 2002
 ### Changed parse_shtml() to remove a warning
-# v1.25 	Tue Mar 11 10:47:36 CST 2003 
+# v1.25 	Tue Mar 11 10:47:36 CST 2003
 ### Updated to be a more generic name - CGI::SHTML.  This will make things
-### a lot easier to distribute.  Have to make a real package now.  Eliminated 
-### the COUNTER stuff, because it's not in use and was silly anyway.  Put 
+### a lot easier to distribute.  Have to make a real package now.  Eliminated
+### the COUNTER stuff, because it's not in use and was silly anyway.  Put
 ### in 'default' values in the headers/footers
-# v1.26		Thu Apr 22 15:00:51 CDT 2004 
-### Making fsize(), flastmod(), etc into internal functions.  
-# v1.26.01	Thu Apr 22 23:32:57 CDT 2004 
+# v1.26		Thu Apr 22 15:00:51 CDT 2004
+### Making fsize(), flastmod(), etc into internal functions.
+# v1.26.01	Thu Apr 22 23:32:57 CDT 2004
 ### Forgot to turn off some debugging information.
-# v1.27		Thu May 06 10:52:32 CDT 2004 
+# v1.27		Thu May 06 10:52:32 CDT 2004
 ### Added if/elif/else/endif functionality.  This was challenging.
 ### Documentation chanes came with it.
-# v1.28		Mon May 17 15:15:22 CDT 2004 
+# v1.28		Mon May 17 15:15:22 CDT 2004
 ### Put back old environment variables after an execute.
-# v1.28		Wed May 19 11:37:06 CDT 2004 
+# v1.28		Wed May 19 11:37:06 CDT 2004
 ### Parsing information is accurate again with parse_shtml - doesn't lose
 ### newlines.  Setting blank versions of those environment variables.
